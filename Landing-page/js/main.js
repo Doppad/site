1934043baba9edc4e82011e6b3c3010e22173acb
Ivan <80860061+Doppad@users.noreply.github.com>
2022-12-06 18:21:52 +0300

    Одностраничный лендинг

---
+++ Landing-page/js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+
+    $('.slider__inner').slick({
+        nextArrow: '<button type="button" class="slick-button slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-button slick-prev"></button>',
+        infinite: false
+    });
+
+$('select').styler();
+
+$('.header__button-menu').on('click',function(){
+    $('.menu ul').slideToggle();
+})
+
+});
